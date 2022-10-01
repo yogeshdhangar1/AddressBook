f4e652c0cab9ccb5bde5a2a73b1f47dfb38271e7
yogeshdhangar5213 <yogeshdhangar5213@gmail.com>
2022-10-01 02:15:33 -0700

    Added:Welcome To AddressBook Program

---
+++ src/main/java/AddressBook.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class AddressBook {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first name");
+       String  fName = sc.next();
+        System.out.println("Enter the last Name");
+         String lName = sc.next();
+        System.out.println("Enter the city");
+        String City = sc.next();
+        System.out.println("Enter the Address");
+        String Address = sc.next();
+        System.out.println("Enter the number");
+        int Number = sc.nextInt();
+        System.out.println("Enter the mail");
+        String Email = sc.next();
+    }
+}
